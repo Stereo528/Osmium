7b19f2b5d5faecc7b5bb8ddce6c5f9f1e4f9489b
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2020-12-04 14:41:30 -0700

    Error is now embed

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from datetime import date
 from datetime import datetime
 
-
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='.', intents=intents, help_command=None)
 today1 = date.today()
@@ -13,17 +12,20 @@ hourString1 = hour1.strftime("%H:%M:%S")
 with open("config.json", "r") as config:
     configloaded = json.load(config)
 
+
 ############
 
-#Load Cogs
+# Load Cogs
 
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f'commands.{extension}')
 
+
 @bot.command()
 async def unload(ctx, extension):
     bot.unload_extension(f'commands.{extension}')
+
 
 @bot.command()
 async def reload(ctx):
@@ -34,13 +36,20 @@ async def reload(ctx):
                 bot.load_extension(f'commands.{filename[:-3]}')
         await ctx.send("Reloaded Cogs")
     except:
-        await ctx.send("Oopsie Woopsie!! The code monkeys at our headquarters are working VEWY HAWD to fix this!")
+        error = discord.Embed(
+            title="Oopsie Woopsie!!1",
+            description="The code monkeys at our headquarters are working VEWY HAWD to fix this!!1!",
+            color=discord.Color.dark_red()
+        )
+        await ctx.send(embed=error)
 
-#load cogs on startup
+
+# load cogs on startup
 for filename in os.listdir('./commands/'):
     if filename.endswith('.py'):
         bot.load_extension(f'commands.{filename[:-3]}')
-        
+
+
 ############
 
 @bot.event
@@ -52,6 +61,7 @@ async def on_command_error(ctx, error):
             description=f"The command `{ctx.message.content.split(' ')[0]}` is not found! Use `.help` to list all commands!")
         await ctx.send(embed=embed)
         return
+
 
 ############
 
