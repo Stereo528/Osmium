8184cb5bf24f66c91aa9e05e688f4483b0447fa3
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2020-12-08 12:31:58 -0700

    Update christmas.py

--- commands/christmas.py
+++ commands/christmas.py
@@ -8,13 +8,13 @@ class Fun(commands.Cog):
 
     @commands.command(aliases=["cm", "xmas"])
     async def christmas(self, ctx):
-        x = randint(0,10)
+        x = randint(0,11)
         y = randint(0,1)
         if y == 0:
             ChrColor = discord.Color.red()
         else:
             ChrColor = discord.Color.green()
-        quotes=["All I want for Christmas is YOUUUUUUUUUUUUUUUU", "*Sings Carol of the Bells*", "HO HO HO", "YOU WANT A SPRITE CRANBERRY", "Its the thirst- Thirstiest time of the YEARRRRRR", "Jingle Bells. Jingle Bells, they don't stop.", "war, war never changes, neither does christmas. is christmas war?", "keep the change you filty animal", "Happy Holidays", "Can it be new years already?", "I enjoy halloween more, can we go back to october?"]
+        quotes=["All I want for Christmas is YOUUUUUUUUUUUUUUUU", "*Sings Carol of the Bells*", "HO HO HO", "YOU WANT A SPRITE CRANBERRY", "Its the thirst- Thirstiest time of the YEARRRRRR", "Jingle Bells. Jingle Bells, they don't stop.", "war, war never changes, neither does christmas. is christmas war?", "keep the change you filty animal", "Happy Holidays", "Can it be new years already?", "I enjoy halloween more, can we go back to october?", "Have a Poggers New Year"]
         ChrEmbed = discord.Embed(
             title="Happy Holidays",
             description=quotes[x],
