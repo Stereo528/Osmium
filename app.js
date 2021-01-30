63f0b998a9aa554a41409f45d76999ed50b90a53
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2021-01-30 16:02:35 -0700

    trying new thing

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-const Discord = require('discord.js');
-const fs = require("fs");
-const Enmap = require("enmap");
-const client = new Discord.Client();
+//const Discord = require('discord.js');
+//const fs = require("fs");
+//const Enmap = require("enmap");
+//const client = new Discord.Client();
+//const config = require("./auth.json");
+
+//client.config = config;
+
+//client.on('ready', () => {
+//    console.log(`Logged in as ${client.user.tag}!`);
+//});
+
+//fs.readdir("./events/", (err, files) => {
+//    if (err) return console.error(err);
+//    files.forEach(file => {
+//    const event = require(`./events/${file}`);
+//    let eventName = file.split(".")[0];
+//    client.on(eventName, event.bind(null, client));
+//    });
+//});
+
+//client.commands = new Enmap();
+
+//fs.readdir("./commands/", (err, files) => {
+//    if (err) return console.error(err);
+//    files.forEach(file => {
+//    if (!file.endsWith(".js")) return;
+//    let props = require(`./commands/${file}`);
+//    let commandName = file.split(".")[0];
+//    console.log(`Attempting to load command ${commandName}`);
+//    client.commands.set(commandName, props);
+//    });
+//});
+
+const Akairo = require('discord-akairo');
+const client = new Akairo.AkairoClient(
+    ownerID="296116954421264384"
+);
+
 const config = require("./auth.json");
 
 client.config = config;
@@ -10,28 +45,13 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-    });
-});
-
-client.commands = new Enmap();
-
-fs.readdir("./commands/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-    });
-});
-
-
+client.on("message", message => {
+    if (message.content.startsWith(config.prefix) === "ping" && message.author.id == ownerID) {
+        message.reply("die")
+    }
+    else if (!message.content.startsWith(config.prefix) && ) {
+        message.reply("help");
+    }
+})
 
 client.login(config.token);
