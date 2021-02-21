f869c943c800349d9fd49e3a2dbc0d9f6b1ecd56
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2021-02-20 22:17:12 -0700

    modmail pt2

--- commands/dm.py
+++ commands/dm.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Example(commands.Cog):
+class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -22,4 +22,4 @@ class Example(commands.Cog):
 
 
 def setup(client):
-    client.add_cog(Example(client))
+    client.add_cog(Admin(client))
