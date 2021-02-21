11d8c1c2da46ded1ecdc2fb644cd41d1e0d342d7
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2021-02-20 21:18:02 -0700

    first part of dm thing

---
+++ commands/dm.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class Example(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def dm(self, ctx, member:discord.Member, *, text):
+        embed = discord.Embed(
+            title="From Osmium",
+            description=text,
+            color=0x123456
+        )
+        embed2 = discord.Embed(
+            title=f"to {member}",
+            description=text,
+            color=0x123456
+        )
+        await member.send(embed=embed)
+        await ctx.send(embed=embed2)
+
+
+def setup(client):
+    client.add_cog(Example(client))
