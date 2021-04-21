7702aee77a278ce5e559c7c2390d1b65f7974379
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2021-04-20 22:22:05 -0600

    about the profile, bot, and server

---
+++ commands/about.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class Util(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=["info"])
+    async def about(self, ctx):
+        infoembed = discord.Embed(
+            title="About",
+            color=0x345678
+        )
+        infoembed.add_field(name="Bot Creator:", value=self.client.get_user(296116954421264384), inline=True)
+        infoembed.add_field(name="Info", value=f"Id: {self.client.user.id}, \nMention: {self.client.user.mention}", inline=False)
+        infoembed.set_thumbnail(url=self.client.user.avatar_url)
+        await ctx.send(embed=infoembed)
+
+def setup(client):
+    client.add_cog(Util(client))
---
+++ commands/serverinfo.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class Util(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def serverinfo(self, ctx):
+        embed = discord.Embed(
+            title=f"Info for {ctx.guild.name}",
+            description=f"Emoji Limit: {ctx.guild.emoji_limit} \nFilesize Limit: {round(ctx.guild.filesize_limit/1000000)} megabytes",
+            color=discord.Color.blurple()
+        )
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        embed.add_field(name="Current Members", value=ctx.guild.member_count, inline=True)
+        embed.add_field(name="Other Data", value=f"Nitro Level: {ctx.guild.premium_tier}, Nitro Boosters: {ctx.guild.premium_subscription_count}", inline=False)
+        embed.add_field(name="Owner Info", value=f"Server Owner: {ctx.guild.owner} \nOwner ID: {ctx.guild.owner_id}", inline=False)
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Util(client))
