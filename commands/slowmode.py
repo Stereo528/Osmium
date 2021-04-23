9548f51bf7f7173925a193932a38170ca0e70451
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2021-04-23 13:07:36 -0600

    Add slowmode command

---
+++ commands/slowmode.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from main import embedCreator
+
+
+class Admin(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=["slow"])
+    async def slowmode(self, ctx, newspeed):
+        if ctx.message.author.guild_permissions.manage_channels:
+            oldSpeed=ctx.channel.slowmode_delay
+            embed=embedCreator("Slowmode Changed", f"from {oldSpeed} to {newSpeed}", 0x00FF00)
+            await ctx.channel.edit(slowmode_delay=newSpeed)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(embed=embedCreator("You Have Insuffcient Perms", "You are missing the \"Manage Channel\" Permissions", 0xFF0000))
+
+
+def setup(client):
+    client.add_cog(Admin(client))
